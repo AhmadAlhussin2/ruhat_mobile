b1c6d4a2ffea7dd1f5061d04574d2f6d1ab097d6
Daber <88327649+daberL@users.noreply.github.com>
2022-06-08 11:28:54 +0300

    class Quiz is added and a function of fetching questions from a remote quiz is implemented

---
+++ lib/models.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:json_annotation/json_annotation.dart';
+
+@JsonSerializable()
+class Quiz {
+  final List<dynamic>questions;
+  Quiz(this.questions);
+  factory Quiz.fromJson(String rawJson) {
+    List<dynamic> questions = jsonDecode(rawJson);
+    return Quiz(questions);
+  }
+}
+
+Future<void> fetchQuestions() async {
+  final uri = Uri.https('daber.space','/api/get_quiz',{'id':"123"});
+  final response = await http.get(uri, headers: {"Access-Control-Allow-Origin": "*"});
+  if (response.statusCode==200){
+    print(Quiz.fromJson(response.body).questions[0]);
+  } else {
+    print("error");
+  }
+}
